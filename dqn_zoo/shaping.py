66570d4af6ad404f2c8ee2553d8028dc6bc99b95
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-03-16 09:52:49 +0000

    placeholders for uncertainty and policy entropy penalty classes

--- dqn_zoo/shaping.py
+++ dqn_zoo/shaping.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 class NoPenalty:
   """No penalty placeholder."""
 
-  def __call__(self, target_params, transitions, rng_key):
+  def __call__(self, target_q_values, transitions, rng_key):
     return transitions.r_t
 
 
@@ -16,9 +16,26 @@ class HardCodedPenalty:
   def __init__(self, penalty: float):
       self._penalty = penalty
 
-  def __call__(self, target_params, transitions, rng_key):
+  def __call__(self, target_q_values, transitions, rng_key):
 
     penalty_terms = self._penalty * jnp.ones_like(transitions.r_t)
     
     return transitions.r_t + penalty_terms
       
+class UncertaintyPenalty:
+  """Adaptive penalty based on uncertainty in state-action values over ensemble."""
+
+  def __init__(self, multiplicative_factor: float):
+    self._multiplicative_factor = multiplicative_factor
+
+  def __call__(self, target_q_values, transitions, rng_key):
+    pass
+
+class PolicyEntropyPenalty:
+  """Adaptive penalty based on policy entropy of ensemble."""
+
+  def __init__(self, multiplicative_factor: float):
+    self._multiplicative_factor = multiplicative_factor
+
+  def __call__(self, target_q_values, transitions, rng_key):
+    pass
