08722f6bf236bc9ca33c9089e4fed1a8e2fedfd3
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-05-17 18:57:41 +0100

    first iteration of munchausen-style log policy penalty. Need to rethink log of Q values

--- dqn_zoo/shaping.py
+++ dqn_zoo/shaping.py
@@ -53,3 +53,21 @@ class PolicyEntropyPenalty:
     penalty_terms = self._multiplicative_factor * self._compute_entropy(max_indices)
     return transitions.r_t + penalty_terms
     
+class MunchausenPenalty:
+  """Adaptive penalty that adds scaled log policy to the reward.
+  
+  Based on M-IQN thinking in Munchausen RL: https://arxiv.org/pdf/2007.14430.pdf
+  """
+
+  def __init__(self, multiplicative_factor: float):
+    self._multiplicative_factor = multiplicative_factor
+
+  def __call__(self, target_q_values, transitions, rng_key):
+
+    mean_target_q_values = jnp.mean(target_q_values, axis=1) # implicit policy for distribution
+    policy = mean_target_q_values[jnp.arange(len(mean_target_q_values)), transitions.a_tm1]
+    log_policy = jnp.log(policy)
+    
+    raise NotImplementedError("Log policy, for negative Q-values? Need to have Q network output log of values?")
+
+    return transitions.r_1 + self._multiplicative_factor * log_policy
