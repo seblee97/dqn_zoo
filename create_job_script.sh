c5abaaa55758529bf570432e17b0d37d4a6fb40e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-06-01 14:53:11 +0100

    ablation

--- create_job_script.sh
+++ create_job_script.sh
@@ -1,2 +1,17 @@
 TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+
+python create_job_script.py --algorithm bootstrapped_dqn --environment gravitar --penalty_type hard_coded_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
 python create_job_script.py --algorithm bootstrapped_dqn --environment gravitar --penalty_type policy_entropy_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment gravitar --penalty_type uncertainty_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+
+python create_job_script.py --algorithm bootstrapped_dqn --environment hero --penalty_type hard_coded_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment hero --penalty_type policy_entropy_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment hero --penalty_type uncertainty_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+
+python create_job_script.py --algorithm bootstrapped_dqn --environment asterix --penalty_type hard_coded_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment asterix --penalty_type policy_entropy_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment asterix --penalty_type uncertainty_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+
+python create_job_script.py --algorithm bootstrapped_dqn --environment defender --penalty_type hard_coded_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment defender --penalty_type policy_entropy_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+python create_job_script.py --algorithm bootstrapped_dqn --environment defender --penalty_type uncertainty_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
