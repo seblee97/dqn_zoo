cad6593cdfb2903c7e2caabf513c23ff6643efd9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-06-01 15:03:38 +0100

    copy of sub dir run script to folder

--- create_job_script.sh
+++ create_job_script.sh
@@ -15,3 +15,5 @@ python create_job_script.py --algorithm bootstrapped_dqn --environment asterix -
 python create_job_script.py --algorithm bootstrapped_dqn --environment defender --penalty_type hard_coded_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
 python create_job_script.py --algorithm bootstrapped_dqn --environment defender --penalty_type policy_entropy_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
 python create_job_script.py --algorithm bootstrapped_dqn --environment defender --penalty_type uncertainty_penalty --conda_env dqn_zoo --save_path job_script --num_cpus 32 --memory 64 --num_hours 72 --exp_name $TIMESTAMP
+
+cp subdir_run.sh results/$TIMESTAMP
