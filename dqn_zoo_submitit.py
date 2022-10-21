fc7da2c6bd124ed0f5f61a30fe0353220bb199a3
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-10-21 10:39:37 +0100

    python commands with path

--- dqn_zoo_submitit.py
+++ dqn_zoo_submitit.py
@@ -1,13 +1,15 @@
+import os
 import subprocess
 
 import submitit
 
 executor = submitit.AutoExecutor(folder="log_exp")
+file_path = os.path.dirname(os.path.abspath(__file__))
 
 executor.update_parameters(timeout_min=120, mem_gb=30, gpus_per_node=1, cpus_per_task=8, slurm_array_parallelism=256, slurm_partition="gpu")
 
 def run_expt(algorithm: str, environment_group: str, environment: str = None, csv_path: str = None, checkpoint_path: str = None):
-    command = f"python -m dqn_zoo.{algorithm}.run_{environment_group}"
+    command = f"python -m {file_path}.dqn_zoo.{algorithm}.run_{environment_group}"
     if environment is not None:
         command = f"{command} --environment_name {environment}"
     if csv_path is not None:
