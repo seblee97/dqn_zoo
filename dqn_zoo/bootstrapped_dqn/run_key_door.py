478ed2d35745ac2784a14457efbb34e6bf8c2dc7
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-11-20 21:14:40 +0100

    change iterations defaults

--- dqn_zoo/bootstrapped_dqn/run_key_door.py
+++ dqn_zoo/bootstrapped_dqn/run_key_door.py
@@ -48,9 +48,9 @@ flags.DEFINE_float("optimizer_epsilon", 0.01 / 32**2, "")
 flags.DEFINE_float("additional_discount", 0.99, "")
 flags.DEFINE_float("max_abs_reward", 1.0, "")
 flags.DEFINE_integer("seed", 1, "")  # GPU may introduce nondeterminism.
-flags.DEFINE_integer("num_iterations", 200, "")
+flags.DEFINE_integer("num_iterations", 500, "")
 flags.DEFINE_integer("num_train_frames", int(1e4), "")  # Per iteration.
-flags.DEFINE_integer("num_eval_frames", int(5e4), "")  # Per iteration.
+flags.DEFINE_integer("num_eval_frames", int(1e4), "")  # Per iteration.
 flags.DEFINE_integer("learn_period", 16, "")
 # flags.DEFINE_string("results_csv_path", "/tmp/results.csv", "")
 # flags.DEFINE_string("checkpoint_path", None, "")
