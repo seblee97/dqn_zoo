594fa58d828f5e9af169dd80030bd4eafd48e9e8
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-27 13:27:18 +0000

    prioritize uncertainty

---
+++ dqn_zoo/prioritize_uncertainty/agent.py
@@ -0,0 +1,415 @@
+"""Implementation of bootstrapped DQN (Osband 2016)"""
+
+from typing import Any, Callable, Mapping, Text
+
+import chex
+import distrax
+import dm_env
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+import rlax
+from absl import logging
+
+from dqn_zoo import parts, processors
+from dqn_zoo import replay as replay_lib
+
+# Batch variant of q_learning.
+_batch_q_learning = jax.vmap(rlax.q_learning)
+_batch_double_q_learning = jax.vmap(rlax.double_q_learning)
+
+
+class BootstrappedDqn(parts.Agent):
+    """Deep Q-Network agent with multiple heads."""
+
+    def __init__(
+        self,
+        preprocessor: processors.Processor,
+        sample_network_input: jnp.ndarray,
+        network: parts.Network,
+        variance_network: bool,
+        optimizer: optax.GradientTransformation,
+        transition_accumulator: Any,
+        replay: replay_lib.TransitionReplay,
+        shaping,
+        mask_probability: float,
+        num_heads: int,
+        batch_size: int,
+        exploration_epsilon: Callable[[int], float],
+        min_replay_capacity_fraction: float,
+        learn_period: int,
+        target_network_update_period: int,
+        grad_error_bound: float,
+        rng_key: parts.PRNGKey,
+    ):
+        self._preprocessor = preprocessor
+        self._replay = replay
+        self._transition_accumulator = transition_accumulator
+        self._mask_probabilities = jnp.array([mask_probability, 1 - mask_probability])
+        self._num_heads = num_heads
+        self._batch_size = batch_size
+        self._exploration_epsilon = exploration_epsilon
+        self._min_replay_capacity = min_replay_capacity_fraction * replay.capacity
+        self._learn_period = learn_period
+        self._target_network_update_period = target_network_update_period
+        self._variance_network = variance_network
+
+        # Initialize network parameters and optimizer.
+        self._rng_key, network_rng_key, var_network_rng_key = jax.random.split(
+            rng_key, 3
+        )
+
+        self._online_params = network.init(
+            network_rng_key, sample_network_input[None, ...]
+        )
+        self._target_params = self._online_params
+        self._opt_state = optimizer.init(self._online_params)
+
+        if variance_network:
+            self._var_online_params = network.init(
+                var_network_rng_key, sample_network_input[None, ...]
+            )
+            self._var_target_params = self._var_online_params
+            self._var_opt_state = optimizer.init(self._var_online_params)
+        else:
+            self._var_online_params = None
+            self._var_target_params = None
+            self._var_opt_state = None
+
+        # Other agent state: last action, frame count, etc.
+        self._action = None
+        self._frame_t = -1  # Current frame index.
+        self._statistics = {"state_value": np.nan}
+
+        LOG_EPSILON = 0.0001
+
+        # Define jitted loss, update, and policy functions here instead of as
+        # class methods, to emphasize that these are meant to be pure functions
+        # and should not access the agent object's state via `self`.
+
+        def _forward(params, state):
+            _, split_key = jax.random.split(rng_key)
+            q_values = network.apply(params, split_key, state).multi_head_output
+            return q_values
+
+        self._forward = jax.jit(_forward)
+
+        def shaping_output(target_params, transitions, rng_key):
+            _, *apply_keys = jax.random.split(rng_key, 3)
+            q_target_t = network.apply(
+                target_params, apply_keys[0], transitions.s_t
+            ).multi_head_output
+            flattened_q_target = jnp.reshape(q_target_t, (-1, q_target_t.shape[-1]))
+            # compute shaping function F(s, a, s')
+            shaped_rewards = shaping_function(q_target_t, transitions, apply_keys[1])
+            penalties = shaped_rewards - transitions.r_t
+
+            return q_target_t, flattened_q_target, shaped_rewards, penalties
+
+        def loss_fn(online_params, target_params, transitions, rng_key):
+            """Calculates loss given network parameters and transitions."""
+            _, *apply_keys = jax.random.split(rng_key, 4)
+            # Batch : Heads : Actions (source state)
+            q_tm1 = network.apply(
+                online_params, apply_keys[0], transitions.s_tm1
+            ).multi_head_output
+            # Batch : Heads : Actions (destination state)
+            q_t = network.apply(
+                online_params, apply_keys[1], transitions.s_t
+            ).multi_head_output
+            # Batch : Heads : Actions (destination state, target network)
+            q_target_t = network.apply(
+                target_params, apply_keys[2], transitions.s_t
+            ).multi_head_output
+
+            # Batch x Heads : Actions
+            flattened_q = jnp.reshape(q_tm1, (-1, q_tm1.shape[-1]))
+            flattened_q_t = jnp.reshape(q_t, (-1, q_t.shape[-1]))
+            flattened_q_target = jnp.reshape(q_target_t, (-1, q_target_t.shape[-1]))
+
+            # Batch x Heads
+            repeated_actions = jnp.repeat(transitions.a_tm1, num_heads)
+            repeated_discounts = jnp.repeat(transitions.discount_t, num_heads)
+
+            if transitions.r_t.shape == repeated_actions.shape:
+                repeated_rewards = transitions.r_t
+            else:
+                repeated_rewards = jnp.repeat(transitions.r_t, num_heads)
+
+            # Batch x Heads
+            raw_td_errors = _batch_double_q_learning(
+                flattened_q,
+                repeated_actions,
+                repeated_rewards,
+                repeated_discounts,
+                flattened_q_target,
+                flattened_q_t,
+            )
+
+            clipped_td_errors = rlax.clip_gradient(
+                raw_td_errors,
+                -grad_error_bound / num_heads,
+                grad_error_bound / num_heads,
+            )
+            losses = rlax.l2_loss(clipped_td_errors)
+            assert losses.shape == (self._batch_size * num_heads,)
+
+            mask = jax.lax.stop_gradient(jnp.reshape(transitions.mask_t, (-1,)))
+            loss = jnp.sum(mask * losses) / jnp.sum(mask)
+            return loss, {"loss": loss, "deltas": raw_td_errors}
+
+        def update(
+            rng_key,
+            opt_state,
+            online_params,
+            target_params,
+            transitions,
+            var_opt_state,
+            var_online_params,
+            var_target_params,
+        ):
+            """Computes learning update from batch of replay transitions."""
+            rng_key, update_key = jax.random.split(rng_key)
+            d_loss_d_params, aux = jax.grad(loss_fn, has_aux=True)(
+                online_params,
+                target_params,
+                transitions,
+                update_key,
+            )
+
+            updates, new_opt_state = optimizer.update(d_loss_d_params, opt_state)
+            new_online_params = optax.apply_updates(online_params, updates)
+
+            if variance_network:
+
+                # create identical transition object with meta-reward
+                var_transitions = replay_lib.MaskedTransition(
+                    s_tm1=transitions.s_tm1,
+                    a_tm1=transitions.a_tm1,
+                    r_t=aux["deltas"] ** 2,
+                    discount_t=transitions.discount_t**2,
+                    s_t=transitions.s_t,
+                    mask_t=transitions.mask_t,
+                )
+
+                rng_key, var_update_key = jax.random.split(rng_key)
+                var_d_loss_d_params, var_aux = jax.grad(loss_fn, has_aux=True)(
+                    var_online_params,
+                    var_target_params,
+                    var_transitions,
+                    var_update_key,
+                )
+
+                var_updates, var_new_opt_state = optimizer.update(
+                    var_d_loss_d_params, var_opt_state
+                )
+                var_new_online_params = optax.apply_updates(
+                    var_online_params, var_updates
+                )
+            else:
+                var_new_opt_state = None
+                var_new_online_params = None
+
+            return (
+                rng_key,
+                new_opt_state,
+                new_online_params,
+                var_new_opt_state,
+                var_new_online_params,
+            )
+
+            # compute expected uncertainty
+            # use gradient transformation wrapper from optax
+            # max_indices = jnp.argmax(q_target_t, axis=-1)
+            # max_index_probabilities = jnp.array(
+            #     [
+            #         jnp.bincount(mi, minlength=6, length=6) / len(max_indices)
+            #         for mi in max_indices
+            #     ]
+            # )
+            # log_max_index_probabilities = jnp.log(max_index_probabilities + LOG_EPSILON)
+            # expected_unc = jnp.sum(
+            #     (max_index_probabilities + LOG_EPSILON) * log_max_index_probabilities,
+            #     axis=1,
+            # )
+
+            # updates, new_opt_state = optimizer.update(d_loss_d_params, opt_state)
+            # new_online_params = optax.apply_updates(online_params, updates)
+            # return (
+            #     rng_key,
+            #     new_opt_state,
+            #     new_online_params,
+            #     loss_values,
+            #     penalties,
+            # )
+
+        self._update = jax.jit(update)
+
+        def select_action(rng_key, network_params, s_t, exploration_epsilon):
+            """Samples action from eps-greedy policy wrt Q-values at given state."""
+            rng_key, apply_key, policy_key = jax.random.split(rng_key, 3)
+
+            network_forward = network.apply(network_params, apply_key, s_t[None, ...])
+            q_t = network_forward.q_values[0]  # average of multi-head output
+
+            # modulate action selection epsilon with uncertainty
+            value_distribution = network_forward.multi_head_output[0]
+            max_indices = jnp.argmax(value_distribution, axis=-1)
+            max_index_probabilities = jnp.bincount(
+                max_indices, minlength=len(q_t), length=len(q_t)
+            ) / len(max_indices)
+            entropy = -jnp.sum(
+                (max_index_probabilities + LOG_EPSILON)
+                * jnp.log(max_index_probabilities + LOG_EPSILON)
+            )
+
+            a_t = distrax.EpsilonGreedy(q_t, exploration_epsilon).sample(
+                seed=policy_key
+            )
+            v_t = jnp.max(q_t, axis=-1)
+            return rng_key, a_t, v_t
+
+        self._select_action = jax.jit(select_action)
+        # self._select_action = select_action
+
+    def _get_random_mask(self, rng_key):
+        return jax.random.choice(
+            key=rng_key, a=2, shape=(self._num_heads,), p=self._mask_probabilities
+        )
+
+    def step(self, timestep: dm_env.TimeStep) -> parts.Action:
+        """Selects action given timestep and potentially learns."""
+        self._frame_t += 1
+
+        timestep = self._preprocessor(timestep)
+
+        # compute unexpected uncertainty measure
+        unexpected_unc = None
+
+        if timestep is None:  # Repeat action.
+            action = self._action
+        else:
+            action = self._action = self._act(timestep)
+
+            for transition in self._transition_accumulator.step(timestep, action):
+                mask = self._get_random_mask(self._rng_key)
+                transition = replay_lib.MaskedTransition(
+                    s_tm1=transition.s_tm1,
+                    a_tm1=transition.a_tm1,
+                    r_t=transition.r_t,
+                    discount_t=transition.discount_t,
+                    s_t=transition.s_t,
+                    mask_t=mask,
+                )
+                self._replay.add(transition)
+
+        if self._replay.size < self._min_replay_capacity:
+            return action, None, None, None
+
+        if self._frame_t % self._learn_period == 0:
+            # loss, shaped_rewards, penalties = self._learn()
+            self._learn()
+            loss = None
+            shaped_rewards = None
+            penalties = None
+        else:
+            loss = None
+            shaped_rewards = None
+            penalties = None
+
+        if self._frame_t % self._target_network_update_period == 0:
+            self._target_params = self._online_params
+
+        return action, loss, shaped_rewards, penalties
+
+    def reset(self) -> None:
+        """Resets the agent's episodic state such as frame stack and action repeat.
+
+        This method should be called at the beginning of every episode.
+        """
+        self._transition_accumulator.reset()
+        processors.reset(self._preprocessor)
+        self._action = None
+
+    def _act(self, timestep) -> parts.Action:
+        """Selects action given timestep, according to epsilon-greedy policy."""
+        s_t = timestep.observation
+        self._rng_key, a_t, v_t = self._select_action(
+            self._rng_key, self._online_params, s_t, self.exploration_epsilon
+        )
+        a_t, v_t = jax.device_get((a_t, v_t))
+        self._statistics["state_value"] = v_t
+        return parts.Action(a_t)
+
+    def _learn(self) -> None:
+        """Samples a batch of transitions from replay and learns from it."""
+        logging.log_first_n(logging.INFO, "Begin learning", 1)
+        transitions = self._replay.sample(self._batch_size)
+        (
+            self._rng_key,
+            self._opt_state,
+            self._online_params,
+            self._var_opt_state,
+            self._var_online_params
+            # loss_values,
+            # shaped_rewards,
+            # penalties,
+        ) = self._update(
+            self._rng_key,
+            self._opt_state,
+            self._online_params,
+            self._target_params,
+            transitions,
+            self._var_opt_state,
+            self._var_online_params,
+            self._var_target_params,
+        )
+        # return loss_values.item(), shaped_rewards.tolist(), penalties.tolist()
+
+    @property
+    def online_params(self) -> parts.NetworkParams:
+        """Returns current parameters of Q-network."""
+        return self._online_params
+
+    @property
+    def statistics(self) -> Mapping[Text, float]:
+        """Returns current agent statistics as a dictionary."""
+        # Check for DeviceArrays in values as this can be very slow.
+        assert all(
+            not isinstance(x, jnp.DeviceArray) for x in self._statistics.values()
+        )
+        return self._statistics
+
+    @property
+    def exploration_epsilon(self) -> float:
+        """Returns epsilon value currently used by (eps-greedy) behavior policy."""
+        return self._exploration_epsilon(self._frame_t)
+
+    def get_state(self) -> Mapping[Text, Any]:
+        """Retrieves agent state as a dictionary (e.g. for serialization)."""
+        state = {
+            "rng_key": self._rng_key,
+            "frame_t": self._frame_t,
+            "opt_state": self._opt_state,
+            "online_params": self._online_params,
+            "target_params": self._target_params,
+            "replay": self._replay.get_state(),
+        }
+        return state
+
+    def set_state(self, state: Mapping[Text, Any]) -> None:
+        """Sets agent state from a (potentially de-serialized) dictionary."""
+        self._rng_key = state["rng_key"]
+        self._frame_t = state["frame_t"]
+        self._opt_state = jax.device_put(state["opt_state"])
+        self._online_params = jax.device_put(state["online_params"])
+        self._target_params = jax.device_put(state["target_params"])
+        self._replay.set_state(state["replay"])
+
+    def forward_all_heads(self, state, variance: bool = False):
+        if variance:
+            params = self._var_online_params
+        else:
+            params = self._online_params
+        return self._forward(state=state, params=params)
--- dqn_zoo/prioritize_uncertainty/run_key_door.py
+++ dqn_zoo/prioritize_uncertainty/run_key_door.py
@@ -1,9 +1,9 @@
-import os
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = 'false'
-
 import collections
+import datetime
 import itertools
+import os
 import sys
+import time
 import typing
 
 import chex
@@ -13,27 +13,29 @@ import jax
 import numpy as np
 import optax
 from absl import app, flags, logging
-from dqn_zoo import atari_data, constants, gym_atari, networks, parts, processors
+from jax.config import config
+
+from dqn_zoo import atari_data, constants, gym_key_door, networks, parts, processors
 from dqn_zoo import replay as replay_lib
 from dqn_zoo import shaping
 from dqn_zoo.bootstrapped_dqn import agent
-from jax.config import config
 
 # Relevant flag values are expressed in terms of environment frames.
 FLAGS = flags.FLAGS
-flags.DEFINE_string("environment_name", "pong", "")
+flags.DEFINE_string("environment_name", "posner_env", "")
 flags.DEFINE_integer("environment_height", 84, "")
 flags.DEFINE_integer("environment_width", 84, "")
 flags.DEFINE_integer("replay_capacity", int(1e4), "")
 flags.DEFINE_bool("compress_state", True, "")
+flags.DEFINE_bool("grayscale", False, "")
 flags.DEFINE_float("min_replay_capacity_fraction", 0.05, "")
 flags.DEFINE_string("shaping_function_type", "no_penalty", "")
 flags.DEFINE_float("shaping_multiplicative_factor", -0.05, "")
 flags.DEFINE_integer("num_heads", 10, "")
 flags.DEFINE_float("mask_probability", 0.25, "")
 flags.DEFINE_integer("batch_size", 32, "")
-flags.DEFINE_integer("max_frames_per_episode", 108000, "")  # 30 mins.
-flags.DEFINE_integer("num_action_repeats", 4, "")
+flags.DEFINE_integer("max_frames_per_episode", 500, "")  # 30 mins.
+flags.DEFINE_integer("num_action_repeats", 1, "")
 flags.DEFINE_integer("num_stacked_frames", 4, "")
 flags.DEFINE_float("exploration_epsilon_begin_value", 1.0, "")
 flags.DEFINE_float("exploration_epsilon_end_value", 0.01, "")
@@ -46,36 +48,67 @@ flags.DEFINE_float("optimizer_epsilon", 0.01 / 32**2, "")
 flags.DEFINE_float("additional_discount", 0.99, "")
 flags.DEFINE_float("max_abs_reward", 1.0, "")
 flags.DEFINE_integer("seed", 1, "")  # GPU may introduce nondeterminism.
-flags.DEFINE_integer("num_iterations", 200, "")
+flags.DEFINE_integer("num_iterations", 500, "")
 flags.DEFINE_integer("num_train_frames", int(1e4), "")  # Per iteration.
-flags.DEFINE_integer("num_eval_frames", int(5e4), "")  # Per iteration.
+flags.DEFINE_integer("num_eval_frames", int(1e4), "")  # Per iteration.
 flags.DEFINE_integer("learn_period", 16, "")
-flags.DEFINE_string("results_csv_path", "./run_results/results.csv", "")
-flags.DEFINE_string("checkpoint_path", "./run_results/checkpoint.pkl", "")
+# flags.DEFINE_string("results_csv_path", "/tmp/results.csv", "")
+# flags.DEFINE_string("checkpoint_path", None, "")
+flags.DEFINE_string("map_ascii_path", "dqn_zoo/key_door_maps/multi_room_bandit.txt", "")
+flags.DEFINE_string("map_yaml_path", "dqn_zoo/key_door_maps/multi_room_bandit.yaml", "")
+flags.DEFINE_integer("env_scaling", 8, "")
+flags.DEFINE_multi_integer("env_shape", (84, 84, 12), "")
+flags.DEFINE_bool(
+    "variance_network", True, ""
+)  # compute direct variance in heads (http://auai.org/uai2018/proceedings/papers/35.pdf)
+flags.DEFINE_integer(
+    "visualise_values", 1, ""
+)  # iteration interval between value function visualisations
+flags.DEFINE_string("results_path", None, "")  # where to store results
 
 
 def main(argv):
-    """Trains Bootstrapped DQN agent on Atari."""
+    """Trains Bootstrapped DQN agent on Key-Door."""
     del argv
     logging.info(
-        "Bootstrapped DQN on Atari on %s.", jax.lib.xla_bridge.get_backend().platform
+        "Bootstrapped DQN on Key-Door on %s.", jax.lib.xla_bridge.get_backend().platform
     )
     random_state = np.random.RandomState(FLAGS.seed)
     rng_key = jax.random.PRNGKey(
         random_state.randint(-sys.maxsize - 1, sys.maxsize + 1)
     )
 
-    if FLAGS.results_csv_path:
-        writer = parts.CsvWriter(FLAGS.results_csv_path)
+    # create timestamp for logging and checkpoint path
+    if FLAGS.results_path is None:
+        raw_datetime = datetime.datetime.fromtimestamp(time.time())
+        exp_timestamp = raw_datetime.strftime("%Y-%m-%d-%H-%M-%S")
+        exp_path = os.path.join("results", exp_timestamp)
+        os.makedirs(exp_path, exist_ok=True)
     else:
-        writer = parts.NullWriter()
+        exp_path = FLAGS.results_path
 
-    def environment_builder():
-        """Creates Atari environment."""
-        env = gym_atari.GymAtari(
-            FLAGS.environment_name, seed=random_state.randint(1, 2**32)
+    visualisation_path = os.path.join(exp_path, "visualisations")
+    os.makedirs(visualisation_path, exist_ok=True)
+
+    def environment_builder(train_index: int = 0, test_index: int = 0):
+        """Creates Key-Door environment."""
+        env = gym_key_door.GymKeyDoor(
+            env_args={
+                constants.MAP_ASCII_PATH: FLAGS.map_ascii_path,
+                constants.MAP_YAML_PATH: FLAGS.map_yaml_path,
+                constants.REPRESENTATION: constants.PIXEL,
+                constants.SCALING: FLAGS.env_scaling,
+                constants.EPISODE_TIMEOUT: FLAGS.max_frames_per_episode,
+                constants.GRAYSCALE: False,
+                constants.BATCH_DIMENSION: False,
+                constants.TORCH_AXES: False,
+            },
+            train_index=train_index,
+            test_index=test_index,
+            env_shape=FLAGS.env_shape,
+            checkpoint_path=exp_path,
         )
-        return gym_atari.RandomNoopsEnvironmentWrapper(
+        return gym_key_door.RandomNoopsEnvironmentWrapper(
             env,
             min_noop_steps=1,
             max_noop_steps=30,
@@ -102,16 +135,26 @@ def main(argv):
             num_pooled_frames=2,
             zero_discount_on_life_loss=True,
             num_stacked_frames=FLAGS.num_stacked_frames,
-            grayscaling=True,
+            grayscaling=FLAGS.grayscale,
         )
 
     # Create sample network input from sample preprocessor output.
-    sample_processed_timestep = preprocessor_builder()(env.reset(True))
+    sample_processed_timestep = preprocessor_builder()(env.reset(train=True))
     sample_processed_timestep = typing.cast(dm_env.TimeStep, sample_processed_timestep)
     sample_network_input = sample_processed_timestep.observation
+
+    if FLAGS.grayscale:
+        num_channels = 1
+    else:
+        num_channels = 3
+
     chex.assert_shape(
         sample_network_input,
-        (FLAGS.environment_height, FLAGS.environment_width, FLAGS.num_stacked_frames),
+        (
+            FLAGS.environment_height,
+            FLAGS.environment_width,
+            num_channels * FLAGS.num_stacked_frames,
+        ),
     )
 
     exploration_epsilon_schedule = parts.LinearSchedule(
@@ -173,6 +216,7 @@ def main(argv):
         preprocessor=preprocessor_builder(),
         sample_network_input=sample_network_input,
         network=network,
+        variance_network=FLAGS.variance_network,
         optimizer=optimizer,
         transition_accumulator=replay_lib.TransitionAccumulator(),
         replay=replay,
@@ -186,7 +230,6 @@ def main(argv):
         target_network_update_period=FLAGS.target_network_update_period,
         grad_error_bound=FLAGS.grad_error_bound,
         rng_key=train_rng_key,
-        variance_network=False
     )
     eval_agent = parts.EpsilonGreedyActor(
         preprocessor=preprocessor_builder(),
@@ -195,9 +238,13 @@ def main(argv):
         rng_key=eval_rng_key,
     )
 
-    # Set up checkpointing.
-    # checkpoint = parts.NullCheckpoint()
-    checkpoint = parts.ImplementedCheckpoint(checkpoint_path=FLAGS.checkpoint_path)
+    # setup writer
+    writer = parts.CsvWriter(os.path.join(exp_path, "writer.csv"))
+
+    # setup checkpointing.
+    checkpoint = parts.ImplementedCheckpoint(
+        checkpoint_path=os.path.join(exp_path, "checkpoint.pkl")
+    )
 
     if checkpoint.can_be_restored():
         checkpoint.restore()
@@ -214,7 +261,12 @@ def main(argv):
 
     while state.iteration <= FLAGS.num_iterations:
         # New environment for each iteration to allow for determinism if preempted.
-        env = environment_builder()
+        if state.iteration == 0:
+            env = environment_builder(train_index=0, test_index=0)
+        else:
+            env = environment_builder(
+                train_index=env.train_index, test_index=env.test_index
+            )
 
         logging.info("Training iteration %d.", state.iteration)
         train_seq = parts.run_loop(train_agent, env, FLAGS.max_frames_per_episode)
@@ -225,10 +277,61 @@ def main(argv):
 
         logging.info("Evaluation iteration %d.", state.iteration)
         eval_agent.network_params = train_agent.online_params
-        eval_seq = parts.run_loop(eval_agent, env, FLAGS.max_frames_per_episode)
+        eval_seq = parts.run_loop(
+            eval_agent, env, FLAGS.max_frames_per_episode, train=False
+        )
         eval_seq_truncated = itertools.islice(eval_seq, FLAGS.num_eval_frames)
         eval_trackers = parts.make_default_trackers(eval_agent)
         eval_stats = parts.generate_statistics(eval_trackers, eval_seq_truncated)
+
+        # visualise value function over environment
+        if state.iteration != 0 and state.iteration % FLAGS.visualise_values == 0:
+
+            # use train agent not eval agent wrapper
+            (
+                raw_state_action_values,
+                state_action_value_means,
+                state_action_value_stds,
+                state_action_value_variance,
+            ) = parts.compute_value_function(
+                train_agent,
+                env,
+                FLAGS.num_stacked_frames,
+                FLAGS.env_shape[:2],
+                variance_network=FLAGS.variance_network,
+            )
+
+            averaged_value_means_position = env.average_values_over_positional_states(
+                state_action_value_means
+            )
+            averaged_value_stds_position = env.average_values_over_positional_states(
+                state_action_value_stds
+            )
+            averaged_value_variance_position = (
+                env.average_values_over_positional_states(state_action_value_variance)
+            )
+
+            env.plot_heatmap_over_env(
+                heatmap=averaged_value_means_position,
+                save_name=os.path.join(
+                    visualisation_path,
+                    f"value_function_mean_{state.iteration}.pdf",
+                ),
+            )
+            env.plot_heatmap_over_env(
+                heatmap=averaged_value_stds_position,
+                save_name=os.path.join(
+                    visualisation_path,
+                    f"value_function_std_{state.iteration}.pdf",
+                ),
+            )
+            env.plot_heatmap_over_env(
+                heatmap=averaged_value_variance_position,
+                save_name=os.path.join(
+                    visualisation_path,
+                    f"value_function_variance_{state.iteration}.pdf",
+                ),
+            )
 
         # Logging and checkpointing.
         human_normalized_score = atari_data.get_human_normalized_score(
