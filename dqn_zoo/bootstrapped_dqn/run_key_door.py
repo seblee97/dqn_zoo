29fc6e852767e6bd5f82e1925d5cb606eb412ec3
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-12-16 16:49:22 +0000

    fewer per it

--- dqn_zoo/bootstrapped_dqn/run_key_door.py
+++ dqn_zoo/bootstrapped_dqn/run_key_door.py
@@ -49,7 +49,7 @@ flags.DEFINE_float("additional_discount", 0.99, "")
 flags.DEFINE_float("max_abs_reward", 1.0, "")
 flags.DEFINE_integer("seed", 1, "")  # GPU may introduce nondeterminism.
 flags.DEFINE_integer("num_iterations", 500, "")
-flags.DEFINE_integer("num_train_frames", int(1e6), "")  # Per iteration.
+flags.DEFINE_integer("num_train_frames", int(1e5), "")  # Per iteration.
 flags.DEFINE_integer("num_eval_frames", int(1e5), "")  # Per iteration.
 flags.DEFINE_integer("learn_period", 4, "")
 # flags.DEFINE_string("results_csv_path", "/tmp/results.csv", "")
