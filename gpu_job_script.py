dbdc793c47362c869e8896c9dd28b849272c7dad
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-04-05 11:05:39 +0100

    game as arg, change run command according to game

--- gpu_job_script.py
+++ gpu_job_script.py
@@ -10,11 +10,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--num_nodes", default=1)
 parser.add_argument("--num_gpus_per_node", default=1)
 parser.add_argument("--mem", default=32)
-parser.add_argument("--timeout", default="0-24:00")
+parser.add_argument("--timeout", default="0-120:00")
+parser.add_argument("--game", default="atari")
 parser.add_argument("--dqn_algorithm", default="bootstrapped_dqn")
 
 
-def _generate_script(num_nodes: int, num_gpus: int, mem: int, timeout: str, algo: str):
+def _generate_script(
+    num_nodes: int, num_gpus: int, mem: int, timeout: str, algo: str, game: str
+):
 
     raw_datetime = datetime.datetime.fromtimestamp(time.time())
     exp_timestamp = raw_datetime.strftime("%Y-%m-%d-%H-%M-%S")
@@ -24,21 +27,30 @@ def _generate_script(num_nodes: int, num_gpus: int, mem: int, timeout: str, algo
     output_path = os.path.join(exp_path, "output.txt")
     error_path = os.path.join(exp_path, "error.txt")
 
-    key_door_map_path = os.path.join(exp_path, "key_door_maps")
-    map_ascii_path = os.path.join(key_door_map_path, "multi_room_bandit.txt")
-    map_yaml_path = os.path.join(key_door_map_path, "multi_room_bandit.yaml")
-    map_yaml_paths = ",".join(
-        [
-            os.path.join(key_door_map_path, yml)
-            for yml in [
-                "multi_room_bandit.yaml",
-                "multi_room_bandit_1.yaml",
-                "multi_room_bandit_2.yaml",
-            ]
-        ]
-    )
+    run_command = f"python -m dqn_zoo.{algo}.run_key_door --results_path={exp_path} "
 
-    shutil.copytree("dqn_zoo/key_door_maps", key_door_map_path)
+    if game == "key_door":
+
+        key_door_map_path = os.path.join(exp_path, "key_door_maps")
+        map_ascii_path = os.path.join(key_door_map_path, "multi_room_bandit.txt")
+        map_yaml_path = os.path.join(key_door_map_path, "multi_room_bandit.yaml")
+        map_yaml_paths = ",".join(
+            [
+                os.path.join(key_door_map_path, yml)
+                for yml in [
+                    "multi_room_bandit.yaml",
+                    "multi_room_bandit_1.yaml",
+                    "multi_room_bandit_2.yaml",
+                ]
+            ]
+        )
+        shutil.copytree("dqn_zoo/key_door_maps", key_door_map_path)
+
+        run_command += (
+            f"--map_ascii_path={map_ascii_path} --map_yaml_path={map_yaml_path} "
+            f"--map_yaml_paths={map_yaml_paths}"
+        )
+
     script_path = os.path.join(exp_path, "script")
 
     with open(script_path, "+w") as script_file:
@@ -51,11 +63,7 @@ def _generate_script(num_nodes: int, num_gpus: int, mem: int, timeout: str, algo
         script_file.write(f"#SBATCH --time={timeout}\n")
         script_file.write(f"#SBATCH --output={output_path}\n")
         script_file.write(f"#SBATCH --error={error_path}\n")
-        script_file.write(
-            f"python -m dqn_zoo.{algo}.run_key_door --results_path={exp_path} "
-            f"--map_ascii_path={map_ascii_path} --map_yaml_path={map_yaml_path} "
-            f"--map_yaml_paths={map_yaml_paths}\n"
-        )
+        script_file.write(f"{run_command}\n")
 
     return script_path
 
@@ -68,5 +76,6 @@ if __name__ == "__main__":
         mem=args.mem,
         timeout=args.timeout,
         algo=args.dqn_algorithm,
+        game=args.game,
     )
     subprocess.call(f"sbatch {script_path}", shell=True)
